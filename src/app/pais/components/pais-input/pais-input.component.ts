06642aa2825dd4ac76e8fe8d0f9bd5bf01cb7dfb
Rodolfo Crisanto <crisanto.rosas.22@gmail.com>
2023-02-17 18:20:01 -0500

    Feat: debounce time en componente input

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,15 +1,30 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pais-input',
   templateUrl: './pais-input.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class PaisInputComponent {
+export class PaisInputComponent implements OnInit {
   @Output() onEnter: EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+
+  dbouncer: Subject<string> = new Subject();
+
+  ngOnInit(): void {
+    this.dbouncer.pipe(debounceTime(300)).subscribe((valor) => {
+      this.onDebounce.emit(valor);
+    });
+  }
+
   termino: string = '';
   buscar() {
     this.onEnter.emit(this.termino);
+  }
+
+  teclaPresionada(event: any) {
+    this.dbouncer.next(this.termino);
   }
 }
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -28,4 +28,8 @@ export class PorPaisComponent {
       },
     });
   }
+
+  sugerencias(termino: string) {
+    this.hayError = false;
+  }
 }
