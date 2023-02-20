08baf63d62478544de402908afd4f81740606724
Rodolfo Crisanto <crisanto.rosas.22@gmail.com>
2023-02-20 12:31:20 -0500

    Feat: clases css condicionales

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -4,8 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styles: [
+    `button {
+      margin-right: 5px;    }`
   ]
 })
 export class PorRegionComponent {
+  regiones: string[] = ['africa', 'americas', 'asia', 'oceania', 'europa'];
+  regionActiva: string = '';
 
+  getClass(region: string) {
+    return (region === this.regionActiva) ? 'btn btn-primary': 'btn btn-outline-primary';
+  }
+
+  activaRegion(region: string) {
+    this.regionActiva = region;
+  }
 }
